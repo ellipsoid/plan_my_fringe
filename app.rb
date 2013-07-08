93dd041048bcd261ca93b2268c5c342f7e75d394
Andrew Northrop <andrew.northrop@gmail.com>
2013-07-08 18:23:58 -0500

    changed config to hopefully work with heroku

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
-require 'sinatra'
+require 'sinatra/base'
+
 
 class App < Sinatra::Base
 
   get '/' do
     File.read(File.join('public', 'show_selector.html'))
   end
+
+  run! if app_file == $0
 
 end
