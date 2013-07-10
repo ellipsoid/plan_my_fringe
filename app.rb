7d148776eeda9982600f249570d95496e149f8a2
Andrew Northrop <andrew.northrop@gmail.com>
2013-07-09 21:58:18 -0500

    added scss to project to replace static stylesheets

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
+require 'sass'
+require 'compass'
 require 'sinatra/base'
-
 
 class App < Sinatra::Base
 
+  configure do
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir = 'stylesheets'
+    end
+
+    set :scss, Compass.sass_engine_options
+    set :views, "views"
+  end
+
   get '/' do
     File.read(File.join('public', 'show_selector.html'))
+  end
+
+  get '/stylesheets/:name.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
   end
 
   run! if app_file == $0
