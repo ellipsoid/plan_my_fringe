aa6f8676c32d4a21476cd510ebe3b6e742c1954d
Andrew Northrop <andrew.northrop@gmail.com>
2014-07-01 19:58:31 -0500

    2014 data imported

--- public/javascripts/festival/festival_controller.js
+++ public/javascripts/festival/festival_controller.js
@@ -1,6 +1,9 @@
 'use strict'
 
 Application.Controllers.controller('FestivalController', function ($scope) {
-  $scope.years = [{string: "2012", url: "#/mnfringe/2012"},
-    {string: "2013", url: "#/mnfringe/2013"}];
+  $scope.years = [
+		{string: "2012", url: "#/mnfringe/2012"},
+    {string: "2013", url: "#/mnfringe/2013"},
+    {string: "2014", url: "#/mnfringe/2014"},
+  ];
 });
